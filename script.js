b0151e09ccc555f8a6aba2d28fd4797523e0936a
Bohdana <bohdana@MacBook-Pro-Apple.local>
2024-04-11 19:29:29 +0300

    optimization

--- script.js
+++ script.js
@@ -6,15 +6,24 @@ const saveTableBtn = document.querySelector('.saveBtn');
 const portraitTableBox = document.getElementById('portraitTableBox');
 
 
-createTableBtn.addEventListener('click', () => {
+function fillImageInfo(el) {
+    return () => {
+        el.textContent = null;
+        let imageElement = document.createElement('img');
+        el.appendChild(imageElement);
+        imageElement.alt = prompt('Association?');
+        imageElement.src = prompt('Download the picture. Enter link:');
+    };
+}
 
+function createNewTable() {
     let newTable = document.createElement('table');
     let caption = document.createElement('caption');
     newTable.appendChild(caption);
     caption.textContent = prompt('Enter name');
-    for (let i = 1; i < 4; i++){
+    for (let i = 1; i < 4; i++) {
         let row = document.createElement('tr');
-        for (let j = 1; j < 4; j++){
+        for (let j = 1; j < 4; j++) {
             let cell = document.createElement('td');
             let cellText = document.createTextNode('Row ' + i + ', Cell ' + j);
             cell.appendChild(cellText);
@@ -25,15 +34,13 @@ createTableBtn.addEventListener('click', () => {
 
     }
     portraitTableBox.appendChild(newTable);
+}
+
+createTableBtn.addEventListener('click', () => {
+    createNewTable();
     let elementsByClassName = document.getElementsByClassName('img-cell');
     for (let el of elementsByClassName) {
-        el.addEventListener('click', () => {
-            el.textContent = null;
-            let imageElement = document.createElement('img');
-            el.appendChild(imageElement);
-            imageElement.alt = prompt('Association?');
-            imageElement.src = prompt('Download the picture. Enter link:');
-        });
+        el.addEventListener('click', fillImageInfo(el));
     }
 });
 
